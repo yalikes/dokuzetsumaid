e30cce928fe8e361c57b0c7126ad64941966ebe4
paohui <2054453988@qq.com>
2020-08-05 15:45:21 +0800

    change src/main.rs to read command, add .gitignore to ignore .lock file and build directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Hello master! Maid detsu.");
+    println!("what can I help you?");
+    let mut command=String::new();
+    io::stdin()
+            .read_line(&mut command)
+            .expect("read command failed, this must be master's reason!");
+    
 }
