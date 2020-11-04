c09251d2a913d58d9fb31929c2c0f9c3db74c44b
paohui <2054453988@qq.com>
2020-11-04 21:46:39 +0800

    partly finish help function

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,33 @@ fn main() {
     println!("Hello master! Maid detsu.");
     println!("what can I help you?");
     let mut command=String::new();
-    io::stdin()
+    loop{
+        io::stdin()
             .read_line(&mut command)
             .expect("read command failed, this must be master's reason!");
-    
+        response(&command);
+    }
+}
+
+fn response(command: &String){
+    let trimed_command=command.trim();
+    if(trimed_command=="h" || trimed_command=="help"){
+        show_help();
+    }
+}
+
+fn show_help(){
+}
+
+struct CommandHelp{
+    command_name: String,
+    description: String,
+}
+impl CommandHelp{
+    fn new(command_name:String,description:String)->CommandHelp{
+        CommandHelp{
+            command_name,
+            description,
+        }
+    }
 }
